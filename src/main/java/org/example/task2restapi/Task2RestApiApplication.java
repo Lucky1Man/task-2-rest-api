ed3a69c661d8d653b14f92a96f7dfae36a56a74f
Orest <oresttsytsyak02@gmail.com>
2024-04-19 09:00:43 +0300

    initial commit

---
+++ src/main/java/org/example/task2restapi/Task2RestApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.task2restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2RestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task2RestApiApplication.class, args);
+    }
+
+}
